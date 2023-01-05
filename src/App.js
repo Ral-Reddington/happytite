bd3aefcee5b7aadffadca1a4fb681a8329fdc6c4
Ral-Reddington <eakabike@yahoo.com>
2023-01-05 21:12:25 +0000

    rals first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/NavBar'
+import bootstrap from "bootstrap"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+function App() {
+  return (
+    <div className='happytite'>
+    <Navbar/> 
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,136 @@
+import React,{useState}from "react";
+//myfirstgit
+import logo from "./img/Logo.svg"
+const Navbar = () => {
+  const[hoverState,setHoverState]=useState(false)
+
+  const handleHoverState=(e)=>{
+    e.target.style.background="#F2994A"
+  }
+
+  const handleHoverStateOut=(e)=>{
+    e.target.style.background=""
+  }
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg" style={{
+        borderBottom:"2px solid #F2C94C", margin: "0 11.75rem 0 11.75rem"
+      }}>
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img src={logo} alt="Happytite Logo"/>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
+          <ul className="navbar-nav mb-2 mb-lg-0">
+              <li className="nav-item" >
+                <span className="nav-link menu-item" aria-current="page" onMouseOver={handleHoverState} onMouseOut={handleHoverStateOut}>
+                  Home
+                </span>
+              </li> 
+              <li className="nav-item">
+              <span className="nav-link menu-item" aria-current="page" onMouseOver={handleHoverState} onMouseOut={handleHoverStateOut}>
+                  Meal planner
+                </span>
+              </li> 
+              <li className="nav-item">
+              <span className="nav-link menu-item" aria-current="page" onMouseOver={handleHoverState} onMouseOut={handleHoverStateOut}>
+                  Diets
+                </span>
+              </li> <li className="nav-item">
+              <span className="nav-link menu-item" aria-current="page" onMouseOver={handleHoverState} onMouseOut={handleHoverStateOut}>
+                  Nutri blog
+                </span>
+              </li> <li className="nav-item">
+              <span className="nav-link menu-item" aria-current="page" onMouseOver={handleHoverState} onMouseOut={handleHoverStateOut}>
+                  Groceries
+                </span>
+              </li> 
+              <li className="nav-item">
+              <button type="button" class="btn btn-outline-success">Sign in</button>
+              </li>
+              </ul>
+            
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
+
+/* 
+<div className="me-auto"> <ul className="navbar-nav mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr className="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+            </div>
+            
+
+
+            <form className="d-flex" role="search">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+            */
