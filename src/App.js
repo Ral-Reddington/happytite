0ced51c9d6aa035b0d99ae27eb5e91bfc425f3a0
Ral-Reddington <eakabike@yahoo.com>
2023-01-08 21:52:10 +0000

    Dropdown achieved

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Home from './components/pages/HomePage';
-import MealPlanner from './components/pages/MealPlanner';
-import Navbar from './components/reuseable-components/NavBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/pages/HomePage";
+import MealPlanner from "./components/pages/MealPlanner";
+import Navbar from "./components/reuseable-components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Diets from "./components/pages/Diets";
 // import 'bootstrap/dist/js/bootstrap.min.js';
 function App() {
-	return (
-		<React.Fragment>
-			<Navbar />
-			<div>
-				<Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='meal-planner' element={<MealPlanner />} />
-				</Routes>
+  return (
+    <React.Fragment>
+      <Navbar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="meal-planner" element={<MealPlanner />} />
+		  <Route path="Diets" element={<Diets/>}/>
+        </Routes>
       </div>
-		</React.Fragment>
-	);
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/components/reuseable-components/NavBar.jsx
+++ src/components/reuseable-components/NavBar.jsx
@@ -1,119 +1,127 @@
 //myfirstgit
-import React, { useState } from 'react';
-import logo from '../img/Logo.svg';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import logo from "../img/Logo.svg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-	// const[hoverState,setHoverState]=useState(false)
+   const[dropDownState,setDropDownState]=useState(false)
 
-	const handleHoverState = (e) => {
-		e.target.style.background = '#F2994A';
-	};
+  const handleHoverState = (e) => {
+    e.target.style.background = "#F2994A";
+  };
 
-	const handleHoverStateOut = (e) => {
-		e.target.style.background = '';
-	};
+  const handleHoverStateOut = (e) => {
+    e.target.style.background = "";
+  };
 
-	return (
-		<>
-			<nav
-				className='navbar navbar-expand-lg'
-				style={{
-					borderBottom: '2px solid #F2C94C',
-					margin: '0 11.75rem 0 11.75rem',
-				}}
-			>
-				<div className='container'>
-					<Link to='/' className='navbar-brand'>
-						<img src={logo} alt='Happytite Logo' />
-					</Link>
-					<button
-						className='navbar-toggler'
-						type='button'
-						data-bs-toggle='collapse'
-						data-bs-target='#navbarSupportedContent'
-						aria-controls='navbarSupportedContent'
-						aria-expanded='false'
-						aria-label='Toggle navigation'
-					>
-						<span className='navbar-toggler-icon'></span>
-					</button>
-					<div
-						className='collapse navbar-collapse d-flex justify-content-end'
-						id='navbarSupportedContent'
-					>
-						<ul className='navbar-nav mb-2 mb-lg-0'>
-							<li className='nav-item'>
-								<Link to='/' style={{ textDecoration: 'none' }}>
-									<span
-										className='nav-link menu-item'
-										aria-current='page'
-										onMouseOver={handleHoverState}
-										onMouseOut={handleHoverStateOut}
-									>
-										Home
-									</span>
-								</Link>
-							</li>
-							<li className='nav-item'>
-								<Link to='meal-planner' style={{ textDecoration: 'none' }}>
-									<span
-										className='nav-link menu-item'
-										aria-current='page'
-										onMouseOver={handleHoverState}
-										onMouseOut={handleHoverStateOut}
-									>
-										Meal planner
-									</span>
-								</Link>
-							</li>
-							<li className='nav-item'>
-								<Link to='diets' style={{ textDecoration: 'none' }}>
-									<span
-										className='nav-link menu-item'
-										aria-current='page'
-										onMouseOver={handleHoverState}
-										onMouseOut={handleHoverStateOut}
-									>
-										Diets
-									</span>
-								</Link>
-							</li>{' '}
-							<li className='nav-item'>
-								<Link to='nutrition-blog' style={{ textDecoration: 'none' }}>
-									<span
-										className='nav-link menu-item'
-										aria-current='page'
-										onMouseOver={handleHoverState}
-										onMouseOut={handleHoverStateOut}
-									>
-										Nutri blog
-									</span>
-								</Link>
-							</li>
-							<li className='nav-item'>
-								<Link to='groceries' style={{ textDecoration: 'none' }}>
-									<span
-										className='nav-link menu-item'
-										aria-current='page'
-										onMouseOver={handleHoverState}
-										onMouseOut={handleHoverStateOut}
-									>
-										Groceries
-									</span>
-								</Link>
-							</li>
-							<li className='nav-item'>
-								<button type='button' className='btn btn-outline-success'>
-									Sign in
-								</button>
-							</li>
-						</ul>
-					</div>
-				</div>
-			</nav>
-		</>
-	);
+  const handleDropDown = () => {
+     setDropDownState(!dropDownState)
+  }
+
+  return (
+    <>
+      <nav
+        className="navbar navbar-expand-lg "
+        style={{
+          borderBottom: "2px solid #F2C94C",
+          margin: "0 11.75rem 0 11.75rem",
+        }}
+      >
+        <div className="container ">
+          <Link to="/" className="navbar-brand">
+            <img src={logo} alt="Happytite Logo" />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse d-flex justify-content-end"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="/" style={{ textDecoration: "none" }}>
+                  <span
+                    className="nav-link menu-item"
+                    aria-current="page"
+                    onMouseOver={handleHoverState}
+                    onMouseOut={handleHoverStateOut}
+                  >
+                    Home
+                  </span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="meal-planner" style={{ textDecoration: "none" }}>
+                  <span
+                    className="nav-link menu-item"
+                    aria-current="page"
+                    onMouseOver={handleHoverState}
+                    onMouseOut={handleHoverStateOut}
+                  >
+                    Meal planner
+                  </span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <div className="position-relative">
+                  <span
+                    className="nav-link menu-item"
+                    aria-current="page"
+                    onMouseOver={handleHoverState}
+                    onMouseOut={handleHoverStateOut}
+					onClick={handleDropDown}
+                  >
+                    Diets
+                  </span>
+                  {dropDownState ? ( <div className="dropdown" >
+                    edet
+                  </div>) :('')}
+                </div>
+              </li>{" "}
+              <li className="nav-item">
+                <Link to="nutrition-blog" style={{ textDecoration: "none" }}>
+                  <span
+                    className="nav-link menu-item"
+                    aria-current="page"
+                    onMouseOver={handleHoverState}
+                    onMouseOut={handleHoverStateOut}
+                  >
+                    Nutri blog
+                  </span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="groceries" style={{ textDecoration: "none" }}>
+                  <span
+                    className="nav-link menu-item"
+                    aria-current="page"
+                    onMouseOver={handleHoverState}
+                    onMouseOut={handleHoverStateOut}
+                  >
+                    Groceries
+                  </span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <button type="button" className="btn btn-outline-success">
+                  Sign in
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Navbar;
