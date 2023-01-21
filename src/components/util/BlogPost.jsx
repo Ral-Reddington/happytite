a543c960088ab8c5f9a1c10094e60ad0d2a25c9f
Ral-Reddington <eakabike@yahoo.com>
2023-01-21 15:12:22 +0000

    final version

--- src/components/util/BlogPost.jsx
+++ src/components/util/BlogPost.jsx
@@ -1,44 +1,43 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import {
-	BsFillBookmarkCheckFill,
-	BsFillBookmarkDashFill,
-} from 'react-icons/bs';
+  BsFillBookmarkCheckFill,
+  BsFillBookmarkDashFill,
+} from "react-icons/bs";
 
-
-const BlogPost = ( { userName, userTitle, content, date, img } ) => {
-  const [ bookmarkState, setBookmarkState ] = useState( false );
+const BlogPost = ({ userName, userTitle, content, date, img }) => {
+  const [bookmarkState, setBookmarkState] = useState(false);
   const handleBookmark = () => {
-		setBookmarkState(!bookmarkState);
-	};
+    setBookmarkState(!bookmarkState);
+  };
   return (
-    <><div className='nutriblog-hover me-5 p-3'>
-    <div className='d-flex  p-3'>
-      <div className='me-5'>
-        <span className='fw-light text-secondary'>
-          {userName} · {userTitle}
-        </span>
-        <br />
-        <span className='fw-bold'>
-          {content}
-        </span>
-        <br />
-        <span className='fw-light text-secondary'>
-          {date} · 10 min read
-        </span>
+    <>
+      <div className="nutriblog-hover me-5 p-3">
+        <div className="d-flex  p-3">
+          <div className="me-5">
+            <span className="fw-light text-secondary">
+              {userName} · {userTitle}
+            </span>
+            <br />
+            <span className="fw-bold">{content}</span>
+            <br />
+            <span className="fw-light text-secondary">
+              {date} · 10 min read
+            </span>
+          </div>
+          <div>
+            <img src={img} alt="the-food" />
+          </div>
+        </div>
+        <div>
+          {!bookmarkState ? (
+            <BsFillBookmarkDashFill onClick={handleBookmark} size={30} />
+          ) : (
+            <BsFillBookmarkCheckFill onClick={handleBookmark} size={30} />
+          )}
+        </div>
       </div>
-      <div>
-        <img src={img} alt='the-food' />
-      </div>
-    </div>
-    <div>
-      {!bookmarkState ? (
-        <BsFillBookmarkDashFill onClick={handleBookmark} size={30}/>
-      ) : (
-        <BsFillBookmarkCheckFill onClick={handleBookmark} size={30}/>
-      )}
-    </div>
-  </div></>
-  )
-}
+    </>
+  );
+};
 
-export default BlogPost
+export default BlogPost;
--- src/components/util/Picture.jsx
+++ src/components/util/Picture.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { useDrag } from 'react-dnd';
 
 const Picture = ( { id, url } ) => {
-  
+  //react dnd code snippet drag
   const [{ isDragging }, drag] = useDrag(() => ({
 		type: 'image',
     item: {id:id},
 		collect: (monitor) => ({
 			isDragging: !!monitor.isDragging(),
 		}),
-  } ) );
+  } ) ); //react dnd code snippet drag
   
   return <img ref={drag} src={url} alt="" srcset="" />
  
